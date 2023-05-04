4451861c299c1392eff18fe4019d5343741f2df0
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-04 16:03:03 +0200

    Update nodeGrVars.groovy

--- vars/nodeGrVars.groovy
+++ vars/nodeGrVars.groovy
@@ -7,6 +7,6 @@ def build(){
   def props2 = readProperties file: 'node.properties'
   env.NODEIMAGE = props2['imageNode']
   sh "echo se ha hecho ${env.NODEIMAGE}"
-  //sh "docker pull ${env.NODEIMAGE}"
-  //sh "docker build ${env.NODEIMAGE}
+  sh "docker pull ${env.NODEIMAGE}"
+  sh "docker build ${env.NODEIMAGE}
 }
