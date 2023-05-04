1199152cc43b2fb7c9f872c79ee109d0c728cb9a
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-04 15:44:53 +0200

    Update nodeGrVars.groovy

--- vars/nodeGrVars.groovy
+++ vars/nodeGrVars.groovy
@@ -1,20 +1,11 @@
-def version(){
-  sh 'node -version'
-}
-
-
 def test(){
   sh 'echo holaholahola2'
 }
-
-
-def setProperties() {
-    def props2 = readProperties file: 'node.properties'
-    env.NODEIMAGE = props2['imageNode']
-    sh "echo se ha hecho ${env.NODEIMAGE}"
-}
-
+ 
 
 def build(){
+  def props2 = readProperties file: 'node.properties'
+  env.NODEIMAGE = props2['imageNode']
+  sh "echo se ha hecho ${env.NODEIMAGE}"
   sh "docker pull ${env.NODEIMAGE}"
 }
