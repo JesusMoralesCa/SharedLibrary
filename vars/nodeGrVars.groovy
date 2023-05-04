645562762f9f29dd04db93b85d9c32fd2e80e765
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-04 17:35:24 +0200

    Update nodeGrVars.groovy

--- vars/nodeGrVars.groovy
+++ vars/nodeGrVars.groovy
@@ -3,14 +3,38 @@ def test(){
 }
  
 
-def build(){
+
+//////////////////////BUILD///////////////////
+def build{
+  def file = readProperties file: 'project.properties'
+  if (file['tecnology'] == 'java'){
+    buildJava()
+    
+  }else if (file['tecnology'] == 'node'){
+    buildNode()
+  
+  }
+}
+///////////////////////////////////////////////
+
+
+///NODE
+def buildNode(){
   def props2 = readProperties file: 'node.properties'
   env.NODEIMAGE = props2['imageNode']
   sh "echo se ha hecho ${env.NODEIMAGE}"
   sh "docker pull ${env.NODEIMAGE}"
   sh "docker build -t ${env.NODEIMAGE}:ImageNode -f DockerF/Dockerfile ."
-  
-  test()
-  
   //sh "docker run -d -p 3000:3000 ImageNode"
 }
+
+///JAVA
+def buildJava(){
+  def props = readProperties file: 'java.properties'
+  env.JAVAIMAGE = props['imageJava']
+  sh "echo se ha hecho ${env.JAVAIMAGE}"
+  sh "docker pull ${env.JAVAIMAGE}"
+  sh "docker build -t ${env.NODEIMAGE}:ImageJava -f DockerJ/Dockerfile ."
+}
+
+
