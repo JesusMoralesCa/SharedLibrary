0e5918610e6d2b0f92ac945dd1c864a905d6efb8
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-08 12:04:29 +0200

    Update nodeGrVars.groovy

--- vars/nodeGrVars.groovy
+++ vars/nodeGrVars.groovy
@@ -24,7 +24,7 @@ def build(){
 ////BuildNode
 
 def checkoutNode(){
-  sh "checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/BuildNode.git']])"
+  checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/BuildNode.git']])
   build2Node()
 }
 
