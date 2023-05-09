bd4c1440a242431e76feced327f687b86473a5e9
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-09 12:09:15 +0200

    Update Node.groovy

--- vars/Node.groovy
+++ vars/Node.groovy
@@ -2,7 +2,6 @@
 
 def call(){
   checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/BuildNode.git']])
-  sh "ls"
   installs()
 }
 
