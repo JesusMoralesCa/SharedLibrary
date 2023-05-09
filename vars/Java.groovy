5fd5e04c1480dee8d2ddd14ed3bad1a984752143
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-09 10:17:44 +0200

    Update Java.groovy

--- vars/Java.groovy
+++ vars/Java.groovy
@@ -1,1 +1,29 @@
+def checkoutJava(){
+  checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/TareaMaven.git']])
+  package()
+}
 
+def packageMVN(){
+  sh "mvn clean package"
+  testMVN()
+}
+
+def testMVN(){
+  sh "mvn test"
+  
+}
+
+def build2Java(){
+  credentials('docker-hub-jesusmoralesc')
+  sh "docker build -t jesusmoralesc/docker-javaMVN ."
+  login()
+}
+
+def login(){
+     sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+    push()
+}
+
+def push(){
+  sh 'docker push jesusmoralesc/docker-javaMVN'
+}
