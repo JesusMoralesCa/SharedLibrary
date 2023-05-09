9e0ba4e25dbbaa786ca74447ba90031125dba855
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-09 11:36:41 +0200

    Update W2Build.groovy

--- vars/W2Build.groovy
+++ vars/W2Build.groovy
@@ -1,8 +1,8 @@
 //////////////////////BUILD///////////////////
-def build(){
+def call(){
   def file = readProperties file: 'project.properties'
   if (file['tecnology'] == 'java'){
-    checkoutJava()
+    nodeGrVars()
     
   }else if (file['tecnology'] == 'node'){
     checkoutNode()
