5db71357eb8e895973548a3a947675d52bbfe8d9
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-12 13:41:48 +0200

    Update WBuild.groovy

--- vars/WBuild.groovy
+++ vars/WBuild.groovy
@@ -2,16 +2,8 @@ def call(){
   def file = readProperties file: 'project.properties'
   checkout scmGit(branches: [[name: '*/test']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/pipeline-template.git']])
   if (file['tecnology'] == 'java'){
-      def pipelineConfig = readYaml(file: 'Java/pipeline.yaml')
-          pipelineConfig.each { stage ->
-            stage(stage.key) {
-              steps {
-                stage.value.steps.each { step ->
-                  // Agregar pasos de la etapa según los valores de 'step'
-                }
-              }
-            }
-          }
+      def pipeline = readYaml(file: 'Java/pipeline.yaml')
+      build(pipeline)
 
   }else if (file['tecnology'] == 'node'){
     load 'Node/pipeline.yaml'
