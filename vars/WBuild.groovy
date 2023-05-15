e4ed71bcca043df1518071a43bb7847dc4ebd4a1
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-15 14:12:13 +0200

    Update WBuild.groovy

--- vars/WBuild.groovy
+++ vars/WBuild.groovy
@@ -6,7 +6,7 @@ def call(){
         for (stageConfig in pipelineConfig.stages) {
           stage(stageConfig.stage) {
             step {
-              for (stepConfig in pipelineConfig.steps) {
+              for (stepConfig in pipelineConfig.step) {
                 
               }
             }
