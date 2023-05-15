b5112378bca73b0a9384498756cf76e41ab77baa
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-15 17:24:46 +0200

    Update WBuild.groovy

--- vars/WBuild.groovy
+++ vars/WBuild.groovy
@@ -2,7 +2,7 @@ import org.foo.*
 
 def call() {
     def file = readProperties file: 'project.properties'
-    checkout scmGit(branches: [[name: '*/test']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/pipeline-template.git']])
+    //checkout scmGit(branches: [[name: '*/test']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/pipeline-template.git']])
     
     if (file['technology'] == 'java') {
         def pipelineConfig = readYaml file: 'Java/pipeline.yaml'
