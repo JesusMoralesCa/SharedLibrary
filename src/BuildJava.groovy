56e6ec1ff537250c4d7da516be1dbeb9e0fe4fa5
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-15 13:41:32 +0200

    Update and rename src/org/foo/DockerBuild.groovy to src/BuildJava.groovy

---
+++ src/BuildJava.groovy
@@ -0,0 +1,20 @@
+class Build {
+  
+  def srcCheckout(){
+    checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/BuildMaven.git']])
+  }
+  
+  def srcMavenCleanPackage(){
+    sh "mvn -B -DskipTests clean package"
+  }
+  
+  def srcMavenTest(){
+    sh "mvn test"
+  }
+
+  def srcDockerBuild(){
+    credentials('docker-hub-jesusmoralesc')
+    sh "docker build -t jesusmoralesc/docker-${env.image} ."
+  }
+
+}
