1da881c0ad2a38a64a6dcb44fccf6af36ea15529
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-16 15:39:10 +0200

    Update srcMavenCleanPackage.groovy

--- src/org/foo/srcMavenCleanPackage.groovy
+++ src/org/foo/srcMavenCleanPackage.groovy
@@ -1,31 +1,12 @@
 package org.foo
 
-import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl
-import org.jenkinsci.plugins.workflow.steps.StepContext
-import org.jenkinsci.plugins.workflow.steps.SynchronousNonBlockingStepExecution
-
-class srcMavenCleanPackage extends AbstractStepImpl {
-
-    void run(StepContext context) {
-        context.get(StepExecutionImpl).apply()
+class srcMavenCleanPackage {
+    void CleanPackage(){
+      sh(mvn -B -DskipTests clean package)
     }
-
-    static class StepExecutionImpl extends SynchronousNonBlockingStepExecution<Void> {
-
-        @Override
-        protected Void run() throws Exception {
-            sh 'mvn -B -DskipTests clean package'
-            return null
-        }
-
-        @Override
-        public org.jenkinsci.plugins.workflow.steps.StepExecution start() throws Exception {
-            StepExecutionImpl execution = new StepExecutionImpl()
-            execution.setResume(run())
-            return execution
-        }
-
-        private static final long serialVersionUID = 1L
+    
+    
+    void run() {
+       CleanPackage()
     }
-
 }
