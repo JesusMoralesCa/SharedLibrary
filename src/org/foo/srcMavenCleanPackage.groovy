495663abee8a4507116f7fd0611f9e800088d1d6
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-16 16:24:07 +0200

    Update srcMavenCleanPackage.groovy

--- src/org/foo/srcMavenCleanPackage.groovy
+++ src/org/foo/srcMavenCleanPackage.groovy
@@ -2,16 +2,20 @@ package org.foo
 
 class srcMavenCleanPackage {
     
-    String script = "sh mvn -B -DskipTests clean package"
+    String script = "mvn -B -DskipTests clean package"
     
     void cleanPackage() {
-      sh "mvn -B -DskipTests clean package"
+        def result = sh(script)
+        
+        if (result == 0) {
+            println "El comando se ejecutó correctamente."
+        } else {
+            println "Error al ejecutar el comando."
+        }
     }
     
     void run() {
         cleanPackage()
     }
 }
-
-
 
