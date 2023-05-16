625114871da23e578a32df40097ba63d248ffad3
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-16 16:09:40 +0200

    Update srcMavenCleanPackage.groovy

--- src/org/foo/srcMavenCleanPackage.groovy
+++ src/org/foo/srcMavenCleanPackage.groovy
@@ -1,16 +1,15 @@
 package org.foo
 
-import jenkins.model.Jenkins
-
 class srcMavenCleanPackage {
-    def script = "mvn -B -DskipTests clean package"
-
+    
+    String script = "mvn -B -DskipTests clean package"
+    
     void cleanPackage() {
-        def sh = Jenkins.instance.createLauncher(null).launch()
-        sh.writeLine(script)
-        sh.waitForOrKill(10000)
+        def process = script.execute()
+        process.waitFor()
+        
     }
-
+    
     void run() {
         cleanPackage()
     }
