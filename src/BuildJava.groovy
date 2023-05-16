b80ef3bd37f0cbaf060e43b0f9d40b2553d23207
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-16 09:21:33 +0200

    Update BuildJava.groovy

--- src/BuildJava.groovy
+++ src/BuildJava.groovy
@@ -3,35 +3,7 @@ package org.foo
 class BuildJava {
   
   
-  def WBuild(){
-    def file = readProperties file: 'project.properties'
-    checkout scmGit(branches: [[name: '*/test']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/pipeline-template.git']])
-    
-    if (file['technology'] == 'java') {
-        def pipelineConfig = readYaml file: 'Java/pipeline.yaml'
-        
-        for (stageConfig in pipelineConfig.stages) {
-            def stageName = stageConfig.stage
-            stage(stageName) {
-                for (stepName in stageConfig.steps) {
-                    //def build = new BuildJava()
-                    build."${stepName}"()
-                    echo "Ejecutando paso: ${stepName}"
-                }
-            }
-        }
-    } else if (file['technology'] == 'node') {
-        def pipelineConfig = readYaml file: 'Node/pipeline.yaml'
-        
-        for (stageConfig in pipelineConfig.stages) {
-            stage(stageConfig.stage) {
-                
-            }
-        }
-    }
 
-  }
-  
   
   def srcCheckout(){
     checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/BuildMaven.git']])
