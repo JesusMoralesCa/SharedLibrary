e34f189541870109293ad704a8c63293ff28c28f
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-16 11:42:02 +0200

    Update srcCheckout.groovy

--- src/org/foo/srcCheckout.groovy
+++ src/org/foo/srcCheckout.groovy
@@ -1,7 +1,7 @@
 class srcCheckout {
-
-}
-
     void run() {
         checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/BuildMaven.git']])
     }
+}
+
+
