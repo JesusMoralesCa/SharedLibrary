edcaa48b6ffa62c4390c748f1ea58434b4939c8b
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-17 12:17:07 +0200

    Update StageGenerator.groovy

--- src/org/foo/StageGenerator.groovy
+++ src/org/foo/StageGenerator.groovy
@@ -8,18 +8,28 @@ class StageGenerator {
       def stageName = stageConfig.stage
       def steps = stageConfig.steps
 
-      def stage = stage(stageName) {
-        script {
-          steps.each { step ->
-            
-            this."$step"()
-          }
-        }
-      }
+      def stage = createStage(stageName, steps)
 
       stages.add(stage)
     }
 
     return stages
+  }
+
+  static createStage(stageName, steps) {
+    return { context, logger ->
+      logger.info("Running stage: $stageName")
+
+      steps.each { step ->
+        // Ejecutar el paso específico de la etapa
+        executeStep(step)
+      }
+    }
+  }
+
+  static executeStep(step) {
+    // Implementar la lógica para ejecutar el paso específico
+    println "Executing step: $step"
+    // ...
   }
 }
