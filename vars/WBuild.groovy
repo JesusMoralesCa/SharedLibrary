17ef243c9c0cfd8e5172c212ef9aca87c5923678
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-17 11:10:19 +0200

    Update WBuild.groovy

--- vars/WBuild.groovy
+++ vars/WBuild.groovy
@@ -13,7 +13,7 @@ def call() {
         for (stageConfig in pipelineConfig.stages) {
             def stageName = stageConfig.stage
             def stepName = stageConfig.steps
-            def build = new stage()
+            def build = new Stage()
             build.run(stageName, stepName)
             
         }
