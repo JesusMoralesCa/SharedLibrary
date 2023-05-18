f0eae67607e85d2e33006a7a7c98f76aecd6b331
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-18 17:35:13 +0200

    Update BuildJava.groovy

--- src/org/foo/BuildJava.groovy
+++ src/org/foo/BuildJava.groovy
@@ -1,25 +1,16 @@
 package org.foo
 
 class BuildJava {
-    def steps = [] // Agrega la propiedad steps y asigna un valor inicial vacío
-
-    def setSteps(List steps) {
-        this.steps = steps
-    }
-
-    def getSteps() {
-        return steps
-    }
-
-    def buildStage(def stage) {
-        // Aquí puedes usar la propiedad steps como desees
-        // Por ejemplo, puedes agregar elementos a la lista de steps
+    def buildStage(String stageName) {
+        def stage = [:]
+        stage.name = stageName
+        
+        def steps = []
         steps.add([
             sh: "mvn -B -DskipTests clean package"
         ])
-
+        stage.steps = steps
         
-
         return stage
     }
 }
