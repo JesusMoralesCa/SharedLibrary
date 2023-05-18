42a4328ffbb3533aae06dbfd8e3595a5ce2c4b43
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-18 09:46:11 +0200

    Update StageGenerator.groovy

--- src/org/foo/StageGenerator.groovy
+++ src/org/foo/StageGenerator.groovy
@@ -1,22 +1,20 @@
 package org.foo
 
 
-  def generateStages(List config) {
-        
-        def stages = config.stages
-        stages.each { stage ->
-            stage(stage.stage) {
-                steps {
-                    stage.steps.each { step ->
-                        script {
-
-                            step()
-                        }
+def generateStages(List config) {
+    def stages = config.stages
+    stages.each { stage ->
+        stage(stage) {
+            steps {
+                stage.steps.each { step ->
+                    script {
+                        step.call() // Llama a la función step
                     }
                 }
             }
         }
-  }
+    }
+}
 
 
 return this
