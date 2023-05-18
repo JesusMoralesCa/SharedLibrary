b68013f07e319e0cd7a3dab283827f7e553dc42d
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-18 16:32:55 +0200

    Update WBuild.groovy

--- vars/WBuild.groovy
+++ vars/WBuild.groovy
@@ -1,22 +1,28 @@
 import org.foo.StageGenerator
 
 def call() {
-
     def file = readProperties file: 'project.properties'
-    checkout scmGit(branches: [[name: '*/test']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/pipeline-template.git']])
+    checkout scmGit(
+        branches: [[name: '*/test']],
+        extensions: [],
+        userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/pipeline-template.git']]
+    )
     
     if (file['tecnology'] == 'java') {
-
-            def pipelineConfig = readYaml file: 'Java/pipeline.yaml'
-            checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/BuildMaven.git']])
-            def stageGenerate = new StageGenerator()
+        def pipelineConfig = readYaml file: 'Java/pipeline.yaml'
+        checkout scmGit(
+            branches: [[name: '*/main']],
+            extensions: [],
+            userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/BuildMaven.git']]
+        )
+        def stageGenerate = new StageGenerator()
         
-            for (stageName in pipelineConfig.stages) {
-                stageGenerate.generateStage(stageName.stage.toString())
-            }
-
-            
-        
+        for (stageName in pipelineConfig.stages) {
+            def stage = stageGenerate.getStage(stageName.stage.toString())
+            if (stage) {
+                stage.script()
+            }  
+        }
     } else if (file['tecnology'] == 'node') {
         def pipelineConfig = readYaml file: 'Node/pipeline.yaml'
         
@@ -27,5 +33,5 @@ def call() {
         }
     }
 }
-return this
 
+return this
