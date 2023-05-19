5285df41b02c566f2648d9df2db6faed6aab1ef0
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-19 12:08:46 +0200

    Update goStage.groovy

--- vars/goStage.groovy
+++ vars/goStage.groovy
@@ -1,11 +1,17 @@
 import org.foo.*
 
 def call() {
-  def jobs = ["JobA", "JobB", "JobC"]
   
- def parallelStagesMap = DinamicStage.generateParallelStages(jobs)
-  
-  parallel parallelStagesMap
+def stages = [
+  new Stage('Stage 1'),
+  new Stage('Stage 2')
+]
 
+  def createStage = new StageD()
+  StageD.stage.execute
+ 
+  
+  
+  
 }
 return this
