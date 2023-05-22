dc6bc1babff6d1e0c70accf8cee2aa35c1698963
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-22 11:58:24 +0200

    Update BuildJava.groovy

--- src/org/foo/BuildJava.groovy
+++ src/org/foo/BuildJava.groovy
@@ -1,5 +1,17 @@
 package org.foo
-import org.foo.Stage
+//import org.foo.Stage
+
+class Stage {
+  String name
+
+  Stage(String name, Closure body) {
+    this.name = name
+    body.delegate = this
+    body.call()
+  }
+}
+
+
 
 class BuildJava {
     String buildName = "buildJava"
