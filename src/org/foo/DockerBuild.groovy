d8a5d98d59e6bbaf465996467f6a5e84ec129ac1
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-22 15:38:31 +0200

    Update DockerBuild.groovy

--- src/org/foo/DockerBuild.groovy
+++ src/org/foo/DockerBuild.groovy
@@ -1,8 +1,17 @@
+//         credentials('docker-hub-jesusmoralesc')
 package org.foo
 
-    def script() {
-         credentials('docker-hub-jesusmoralesc')
-         sh "docker build -t jesusmoralesc/docker-${env.image} ."
+class DockerBuild {
+    private final Script script
+
+    DockerBuild(Script script) {
+        this.script = script
     }
-    
-return this
+
+        void execute(String name) {
+        script.stage(name) {
+            script.echo "Triggering ${name} stage..."
+            script.sh "docker build -t jesusmoralesc/docker-${env.image} ."
+        }    
+    }
+}
