8cb31553aa982c1141ad97a0e78cc5c1460b563a
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-22 16:03:17 +0200

    Update StageGenerator.groovy

--- src/org/foo/StageGenerator.groovy
+++ src/org/foo/StageGenerator.groovy
@@ -8,6 +8,7 @@ class StageGenerator {
         try {
             stageClassName = "${StageGenerator.class.packageName}.$stage"
             objectInstance = Class.forName(stageClassName, true, Thread.currentThread().contextClassLoader)
+            objectInstance.execute(stageClassName)
             
             
         } catch (ClassNotFoundException ex) {
