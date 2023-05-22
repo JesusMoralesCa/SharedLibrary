500e06ca0d3c375887592bc384b04199b88325bf
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-22 16:33:53 +0200

    Update StageGenerator.groovy

--- src/org/foo/StageGenerator.groovy
+++ src/org/foo/StageGenerator.groovy
@@ -3,7 +3,7 @@ package org.foo
 class StageGenerator {
     def getStage(String stage) {
         def objectInstance
-        def stageClassName
+        String stageClassName
 
         try {
             stageClassName = "${StageGenerator.class.packageName}.$stage"
