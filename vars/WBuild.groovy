e7c91f4fed7b2ce3c33e6f31c700c903ecba1e67
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-22 17:28:58 +0200

    Update WBuild.groovy

--- vars/WBuild.groovy
+++ vars/WBuild.groovy
@@ -20,10 +20,8 @@ def call() {
         for (stageName in pipelineConfig.stages) {
             //def stage = stageGenerate.getStage(stageName.stage.toString())
             //def stage = new BuildJava(this)
-            def stage = stageName.stage.toString()
-            def objectInstance = this.getClass().getClassLoader().loadClass(stage).newInstance()
 
-            objectInstance.execute(stageName.stage.toString())
+            stage.execute(stageName.stage.toString())
             
             
         }
