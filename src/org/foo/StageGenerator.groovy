b63d5a828ff0c7e221b9f882d8b972f89e7016ea
Jesus Morales <jesus.morales.carr@gmail.com>
2023-05-22 18:09:01 +0200

    fix the script

--- src/org/foo/StageGenerator.groovy
+++ src/org/foo/StageGenerator.groovy
@@ -6,19 +6,21 @@ class StageGenerator {
     StageGenerator(script) {
         this.script = script
     }
+
+
     def getStage(String stage) {
         def objectInstance
         def stageClassName
 
         try {
             stageClassName = "${StageGenerator.class.packageName}.$stage"
-            this.echo("ClassName: ${stageClassName}")
+            script.echo("ClassName: ${stageClassName}")
             objectInstance = Class.forName(stageClassName, true, Thread.currentThread().contextClassLoader).newInstance()
             
             
             
         } catch (ClassNotFoundException ex) {
-            this.echo("No se ha encontrado la stage")
+            script.echo("No se ha encontrado la stage")
         }
         
         return objectInstance
