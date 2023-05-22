c2fd9ee2b377f4a352a6df9e233889ed8c341c0a
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-22 09:40:54 +0200

    Update WBuild.groovy

--- vars/WBuild.groovy
+++ vars/WBuild.groovy
@@ -19,10 +19,7 @@ def call() {
         
         for (stageName in pipelineConfig.stages) {
             def stage = stageGenerate.getStage(stageName.stage.toString())
-            stage.execute {
-                    // Definir las acciones específicas de esta etapa
-                echo "Ejecutando etapa: ${stage.name}"
-            }
+            stage.build()
             
         }
     } else if (file['tecnology'] == 'node') {
