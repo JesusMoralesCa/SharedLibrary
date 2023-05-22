da51e293d27dbb504613c0d3afbb2678239901aa
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-22 09:47:49 +0200

    Update BuildJava.groovy

--- src/org/foo/BuildJava.groovy
+++ src/org/foo/BuildJava.groovy
@@ -10,13 +10,6 @@ class BuildJava{
     void build() {
         def stage = new Stage(name) {
             def process = "mvn -B -DskipTests clean package".execute()
-            process.waitFor()
-
-            if (process.exitValue() == 0) {
-                println "El proceso de construcción se completó exitosamente."
-            } else {
-                println "Ocurrió un error durante el proceso de construcción."
-            }
         }
     }
 }
