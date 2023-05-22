453be4c5f5a33ff851d2b050d9f1d7cce1e8a885
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-22 10:43:54 +0200

    Update BuildJava.groovy

--- src/org/foo/BuildJava.groovy
+++ src/org/foo/BuildJava.groovy
@@ -5,10 +5,8 @@ import org.foo.Stage
 class BuildJava{
     String name
     
-     BuildJava(String name){
-       this.name = name
-    }
-    void build() {
+    BuildJava(String name) {
+        this.name = name
         def stageBuild = new Stage(name) {
             def process = "mvn -B -DskipTests clean package".execute()
         }
