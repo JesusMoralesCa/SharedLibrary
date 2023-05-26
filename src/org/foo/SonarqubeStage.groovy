bb2ab845557b370066de097d815c856e65496790
Jesus Morales <jesus.morales.carr@gmail.com>
2023-05-26 09:24:08 +0200

    Update SonarqubeStage

--- src/org/foo/SonarqubeStage.groovy
+++ src/org/foo/SonarqubeStage.groovy
@@ -1,5 +1,7 @@
 package org.foo
 
+import java.util.Properties
+
 class SonarqubeStage {
     public Script script
 
@@ -12,6 +14,11 @@ class SonarqubeStage {
         script.stage(name) {
             script.echo "Triggering ${name} stage..."
             script.withSonarQubeEnv("sonarqube") {
+
+            def sonarProps = new Properties()
+            script.readFile('sonar-project.properties') { propertiesContent ->
+                sonarProps.load(new StringReader(propertiesContent))
+            }
 
             def sonarProps = readProperties file: 'sonar-project.properties'
             script.sh "sonar-scanner -Dsonar.sources=${sonarProps.getProperty('sonar.sources')}"
