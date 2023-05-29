1302273b4e99cefe354d3e7d2044af8982d56512
Jesus Morales <jesus.morales.carr@gmail.com>
2023-05-29 13:43:29 +0200

    Update Test

--- test/src/org/foo/StageGeneratorSpec.groovy
+++ test/src/org/foo/StageGeneratorSpec.groovy
@@ -2,7 +2,7 @@ package org.foo
 
 import com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification
 
-class StageGeneratorSpec extends JenkinsPipelineSpecification{
+class StageGeneratorSpec extends JenkinsPipelineSpecification {
 
     def "Get instance"() {
         given:
@@ -18,6 +18,28 @@ class StageGeneratorSpec extends JenkinsPipelineSpecification{
 
         when:
         def stage = factory.getStage("Maven", "Build")
+
+        then:
+        stage != null
+    }
+
+    def "Get stage - Maven"() {
+        given:
+        def factory = StageFactory.instance
+
+        when:
+        def stage = factory.getStage("Java", "Test")
+
+        then:
+        stage != null
+    }
+
+    def "Get stage - Docker"() {
+        given:
+        def factory = StageFactory.instance
+
+        when:
+        def stage = factory.getStage("Docker", "Build")
 
         then:
         stage != null
