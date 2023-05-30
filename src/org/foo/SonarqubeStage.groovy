b62b5086bd553587c21d82ae9e1483bf41c3b0df
Jesus Morales <jesus.morales.carr@gmail.com>
2023-05-30 12:25:41 +0200

    try3

--- src/org/foo/SonarqubeStage.groovy
+++ src/org/foo/SonarqubeStage.groovy
@@ -18,7 +18,7 @@ class SonarqubeStage {
             script.echo "Triggering ${name} stage..."
             def scannerHome = script.tool 'SonarScanner'
             script.withSonarQubeEnv("sonarqube") {
-                    script.sh "${scannerHome}/bin/sonar-scanner"
+                    script.sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${sonarProps['sonar.projectKey']} -Dsonar.sources=${sonarProps['sonar.sources']}"
                 
             }
         }
