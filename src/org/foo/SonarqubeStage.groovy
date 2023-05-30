2df2f5fff02fd93a9a7b3eb234f7f52cd68a1c2a
Jesus Morales <jesus.morales.carr@gmail.com>
2023-05-30 12:45:10 +0200

    Test

--- src/org/foo/SonarqubeStage.groovy
+++ src/org/foo/SonarqubeStage.groovy
@@ -9,6 +9,8 @@ class SonarqubeStage {
 
     void execute(String name) {
         def sonarProps = [
+            'sonar.host.url': 'http://172.21.0.1:9000',
+            'sonar.login': 'sonarqube',
             'sonar.projectKey': 'pipeline-template',
             'sonar.sources': 'src',
             'sonar.language': 'groovy'
@@ -17,7 +19,7 @@ class SonarqubeStage {
         script.stage(name) {
             script.echo "Triggering ${name} stage..."
             def scannerHome = script.tool 'SonarScanner'
-            script.withSonarQubeEnv("sonarqube") {
+            script.withSonarQubeEnv(sonarProps) {
                     script.sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${sonarProps['sonar.projectKey']} -Dsonar.sources=${sonarProps['sonar.sources']} -Dsonar.language=${sonarProps['sonar.language']}"
                 
             }
