de7b5d33fdc464ac21491b8155ca91a6f69cc57d
Jesus Morales <jesus.morales.carr@gmail.com>
2023-05-30 13:24:19 +0200

    Test for sonarqube

--- src/org/foo/SonarqubeStage.groovy
+++ src/org/foo/SonarqubeStage.groovy
@@ -7,7 +7,7 @@ class SonarqubeStage {
         this.script = script
     }
 
-    void execute(String name) {
+void execute(String name) {
         def sonarProps = [
             'sonar.host.url': 'http://172.21.0.1:9000',
             'sonar.login': 'squ_34a79036fb8a3e556be4c083e435e5cc9c523967',
@@ -19,9 +19,10 @@ class SonarqubeStage {
         script.stage(name) {
             script.echo "Triggering ${name} stage..."
             def scannerHome = script.tool 'SonarScanner'
-            script.withSonarQubeEnv() {
-                    script.sh "${scannerHome}/bin/sonar-scanner -Dsonar.host.url=${sonarProps['sonar.host.url']} -Dsonar.projectKey=${sonarProps['sonar.projectKey']} -Dsonar.login=${sonarProps['sonar.login']} -Dsonar.sources=${sonarProps['sonar.sources']} -Dsonar.language=${sonarProps['sonar.language']}"
-                
+            script.withSonarQubeEnv(credentialsId: sonarProps['sonar.login'], installationName: 'SonarQube') {
+                script.withEnv(["SONAR_HOST_URL=${sonarProps['sonar.host.url']}", "SONAR_PROJECT_KEY=${sonarProps['sonar.projectKey']}", "SONAR_SOURCES=${sonarProps['sonar.sources']}", "SONAR_LANGUAGE=${sonarProps['sonar.language']}"]) {
+                    script.sh "${scannerHome}/bin/sonar-scanner"
+                }
             }
         }
     }
