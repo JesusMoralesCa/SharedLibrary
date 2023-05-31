f3334b5e9357adce1b4abbdfe82c7af99eede517
Jesus Morales <jesus.morales.carr@gmail.com>
2023-05-31 13:33:09 +0200

    fix

--- src/org/foo/StageUtils/StageGenerator.groovy
+++ src/org/foo/StageUtils/StageGenerator.groovy
@@ -17,20 +17,19 @@ class StageGenerator {
             script.echo("ClassName: ${stageClassName}")
             objectInstance = Class.forName(stageClassName, true, Thread.currentThread().contextClassLoader).newInstance()
             objectInstance.script = script
-
-            if (!objectInstance.exists) {
-            
-                try {
-                    stageClassName = "org.foo.Stages.$stage.$stage"
-                    objectInstance = Class.forName(stageClassName, true, Thread.currentThread().contextClassLoader)
-                    objectInstance.script = script
-            } catch (ClassNotFoundException ex) {
-                    script.echo("No se ha encontrado la stage por segunda vez")
-                }
-            }
             
         } catch (ClassNotFoundException ex) {
             script.echo("No se ha encontrado la stage")
+        }
+
+        if (!objectInstance.exists) {
+            try {
+                stageClassName = "org.foo.Stages.$stage.$stage"
+                objectInstance = Class.forName(stageClassName, true, Thread.currentThread().contextClassLoader)
+                objectInstance.script = script
+            } catch (ClassNotFoundException ex) {
+                script.echo("No se ha encontrado la stage por segunda vez")
+            }
         }
 
         return objectInstance
