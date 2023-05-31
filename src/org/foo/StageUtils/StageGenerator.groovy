7b6e13a70aa0850f32b54bff975ceffd4dc6e805
Jesus Morales <jesus.morales.carr@gmail.com>
2023-05-31 13:18:03 +0200

    Fix code

--- src/org/foo/StageUtils/StageGenerator.groovy
+++ src/org/foo/StageUtils/StageGenerator.groovy
@@ -13,7 +13,7 @@ class StageGenerator {
         def stageClassName
 
         try {
-            stageClassName = "${StageGenerator.class.packageName}.$stage.$stage$Technology"
+            stageClassName = "org.foo.Stages.$stage.$stage$Technology"
 
             script.echo("ClassName: ${stageClassName}")
             objectInstance = Class.forName(stageClassName, true, Thread.currentThread().contextClassLoader).newInstance()
@@ -27,10 +27,10 @@ class StageGenerator {
         if (!objectInstance) {
             
             try {
-                stageClassName = "${StageFactory.class.packageName}.$stage.$stage"
+                stageClassName = "org.foo.Stages.$stage.$stage"
                 objectInstance = Class.forName(stageClassName, true, Thread.currentThread().contextClassLoader)
             } catch (ClassNotFoundException ex) {
-                print("WARN: Is not possible to get the needed Stage")
+                script.echo("No se ha encontrado la stage por segunda vez")
             }
         }
 
