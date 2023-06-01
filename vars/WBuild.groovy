8b41515f6b21b73fc81d9a19638caddf18698266
Jesus Morales <jesus.morales.carr@gmail.com>
2023-06-01 09:51:22 +0200

    Update

--- vars/WBuild.groovy
+++ vars/WBuild.groovy
@@ -2,7 +2,7 @@ import org.foo.StageUtils.*
 import org.foo.Stages.CodeScan.*
 
 def call() {
-    
+
     def file = readProperties file: 'project.properties'
     checkout scmGit(
         branches: [[name: '*/main']],
@@ -10,13 +10,8 @@ def call() {
         userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/pipeline-template.git']]
     )
 
-        def pipelineConfig = readYaml file: "${file['tecnology']}/pipeline.yaml"
-        checkout scmGit(
-            branches: [[name: '*/main']],
-            extensions: [],
-            userRemoteConfigs: [[url: "https://github.com/JesusMoralesCa/Build${file['tecnology']}.git"]]
-        )
-        def stageGenerate = new StageGenerator(this)
+    def pipelineConfig = readYaml file: "${file['tecnology']}/pipeline.yaml"
+    def stageGenerate = new StageGenerator(this)
         
     for (stageName in pipelineConfig.stages) {
         def stageB = stageGenerate.getStage(stageName.stage.toString(), "${file['tecnology']}")
